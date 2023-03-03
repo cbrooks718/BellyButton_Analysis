3c4660a52f9211cf564b0bc8ee4a9c4d7ce5fb64
cbrooks718 <charliebrooks@Charlies-MacBook-Air.local>
2023-03-03 14:41:08 -0600

    Edits pt. 2, deliverable 1

--- charts.js
+++ charts.js
@@ -63,7 +63,7 @@ function buildCharts(sample) {
     // Deliverable 1: 3. Create a variable that holds the samples array. 
     var samples = data.samples;
     // Deliverable 1: 4. Create a variable that filters the samples for the object with the desired sample number.
-    var sampleArray = sampleData.filter(sampleObj => sampleObj.id == sample);
+    var sampleArray = samples.filter(sampleObj => sampleObj.id == sample);
     // Deliverable 3: 1. Create a variable that filters the metadata array for the object with the desired sample number.
 
     // Deliverable 1: 5. Create a variable that holds the first sample in the array.
